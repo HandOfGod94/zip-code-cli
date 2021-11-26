807c12a45024cb0311bb710a5a7015de5be8bfbb
Gahan Rakholia <gahan94rakh@gmail.com>
2021-11-27 02:07:35 +0530

    add basic cli options

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-http "3.12.3"]
                  [com.gfredericks/vcr-clj "0.4.22"]
-                 [cheshire "5.10.1"]]
+                 [cheshire "5.10.1"]
+                 [org.clojure/tools.cli "1.0.206"]]
   :main ^:skip-aot zip-code-cli.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/zip_code_cli/core.clj
+++ src/zip_code_cli/core.clj
@@ -1,7 +1,29 @@
 (ns zip-code-cli.core
+  (:require [clojure.tools.cli :as cli]
+            [clojure.string :as string])
   (:gen-class))
 
+(def cli-options [["-c" "--zip-code ZIPCODE" "Zip code of a US state"
+                   :default 000000]
+                  ["-h", "--help", "display help for the command"]])
+
+(defn  usage-summary [option-summary]
+  (->> ["A small CLI app written in clojure to help out with zipcodes"
+        ""
+        "Usage: zip [option] code"
+        ""
+        option-summary]
+       (string/join \newline)))
+
+(defn parse [args]
+  (let [{:keys [options arguments errors summary]} (cli/parse-opts args cli-options)]
+    (cond
+      (:help options) {:message (usage-summary summary)}
+      errors {:message (format "Ooops!! Something went wrong \n %s" summary)}
+      (:zip-code options) {:message (format "Fetching info for zip-code: %s" (:zip-code options))})))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [message (:message (parse args))]
+    (println message)))
